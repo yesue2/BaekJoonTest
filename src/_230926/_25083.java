47c2f719d9675ecde9289532e3ab50274e499b93
yeseu2 <ysbjh2464@naver.com>
2023-09-26 20:24:05 +0900

    success

--- src/_230926/_25083.java
+++ src/_230926/_25083.java
@@ -1,2 +1,12 @@
-package _230926;public class _25083 {
+package _230926;
+
+public class _25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
 }
