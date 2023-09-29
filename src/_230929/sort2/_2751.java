eeca6ad2be1cc17a92673f6920c6210198217198
YeSeul <ysbsjh2464@naver.com>
2023-09-29 19:18:59 +0900

    test1

---
+++ src/_230929/sort2/_2751.java
@@ -0,0 +1,12 @@
+package _230929.sort2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
