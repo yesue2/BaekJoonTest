aefec1e849f124497596de749db4429945d0852a
YeSeul <ysbsjh2464@naver.com>
2023-10-04 21:18:47 +0900

    1181 success

---
+++ src/_231004/Sort4/_1181.java
@@ -0,0 +1,42 @@
+package _231004.Sort4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class _1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        String[] words = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            words[i] = br.readLine();
+        }
+
+        Arrays.sort(words, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+
+        sb.append(words[0] + '\n');
+
+        for (int i = 1; i < n; i++) {
+            if(!words[i].equals(words[i - 1])) {
+                sb.append(words[i] + '\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
