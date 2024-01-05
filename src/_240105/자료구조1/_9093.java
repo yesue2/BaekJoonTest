4521af535ecb1371334057b4bc66334dc1c023a1
YeSeul <ysbsjh2464@naver.com>
2024-01-05 18:03:26 +0900

    9093 success

--- src/_240105/자료구조1/_9093.java
+++ src/_240105/자료구조1/_9093.java
@@ -3,15 +3,21 @@ package _240105.자료구조1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class _9093 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
         int t = Integer.parseInt(br.readLine());
 
+        for (int i = 0; i < t; i++) {
+            String[] words = br.readLine().split(" ");
+
+            for (String word : words) {
+                StringBuilder sb = new StringBuilder(word);
+                System.out.print(sb.reverse().append(' '));
+            }
+            System.out.println();
+        }
     }
 }
