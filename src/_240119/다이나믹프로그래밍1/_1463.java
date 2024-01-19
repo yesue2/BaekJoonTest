ba1943b877a06fdb50d72206f4955262e87d098b
YeSeul <ysbsjh2464@naver.com>
2024-01-19 14:34:09 +0900

    1463 success

---
+++ src/_240119/다이나믹프로그래밍1/_1463.java
@@ -0,0 +1,43 @@
+package _240119.다이나믹프로그래밍1;
+
+import java.io.*;
+
+public class _1463 {
+    static Integer[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        dp = new Integer[N + 1];
+        dp[0] = dp[1] = 0;
+
+
+        bw.write(recur(N) + "\n");
+        bw.flush();
+        bw.close();
+    }
+
+    static int recur(int N) {
+
+        if (dp[N] == null) {
+            // 6으로 나눠지는 경우
+            if (N % 6 == 0) {
+                dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
+            }
+            // 3으로만 나눠지는 경우
+            else if (N % 3 == 0) {
+                dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
+            }
+            // 2로만 나눠지는 경우
+            else if (N % 2 == 0) {
+                dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
+            }
+            // 2와 3으로 나누어지지 않는 경우
+            else {
+                dp[N] = recur(N - 1) + 1;
+            }
+        }
+        return dp[N];
+    }
+}
