facaa7ee48d4c241c8086e24ca5a3d6bfeca7e4a
YeSeul <ysbsjh2464@naver.com>
2024-01-23 01:08:15 +0900

    3085 processing

---
+++ src/_240131/브루트포스/_3085.java
@@ -0,0 +1,18 @@
+package _240131.브루트포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _3085 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String[] contents = new String[N];
+            contents[i] = br.readLine();
+        }
+    }
+}
