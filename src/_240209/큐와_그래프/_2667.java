31b81a70c4ef37b491418855bb1e7ea28e5eab86
YeSeul <ysbsjh2464@naver.com>
2024-02-09 16:20:35 +0900

    2667 processing

---
+++ src/_240209/큐와_그래프/_2667.java
@@ -0,0 +1,13 @@
+package _240209.큐와_그래프;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2667  {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+
+}
