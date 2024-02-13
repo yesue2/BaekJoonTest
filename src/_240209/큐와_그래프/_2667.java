94fa5cfcfb1dc39840db26d9249d0f372a98ae1e
YeSeul <ysbsjh2464@naver.com>
2024-02-13 17:08:51 +0900

    2667 success

--- src/_240209/큐와_그래프/_2667.java
+++ src/_240209/큐와_그래프/_2667.java
@@ -1,25 +1,22 @@
 package _240209.큐와_그래프;
 
-import java.awt.image.ImageProducer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class _2667  {
     static int N;
     static int[][] map;
     static int houseCnt;
-    static boolean[][] visited;
-    static StringBuilder sb = new StringBuilder();
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        map = new int[N+1][N+1];
-        visited = new boolean[map.length][map.length];
+        map = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             String tmp = br.readLine();
@@ -29,37 +26,35 @@ public class _2667  {
 
         }
 
+        ArrayList<Integer> arr = new ArrayList<>();
         int complexCnt = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (visited[i][j] == true)
-                    continue;
-                bfs(i, j);
-                sb.append(houseCnt).append('\n');
-                complexCnt++;
-            }
-        }
-
-        System.out.println(complexCnt);
-        System.out.println(sb);
-    }
-    static void bfs(int i, int j) {
-        Queue<Integer> queue = new LinkedList<>();
-        houseCnt = 1;
-
-        queue.add(map[i][j]);
-        visited[i][j] = true;
-
-        while (!queue.isEmpty()) {
-            int v = queue.poll();
-            for (int k = 0; k < N; k++) {
-                if (!visited[v][k] && map[v][k] == 1) {
-                    queue.add(k);
-                    visited[v][k] = true;
-                    houseCnt++;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1) {
+                    houseCnt = 0;
+                    dfs(i, j);
+                    arr.add(houseCnt);
+                    complexCnt++;
                 }
             }
         }
-    }
 
+        Collections.sort(arr);
+        System.out.println(complexCnt);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+    static void dfs(int x, int y) {
+        map[x][y] = 0;
+        houseCnt++;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < N && ny >= 0 && ny < N && map[nx][ny] == 1)
+                dfs(nx, ny);
+        }
+    }
 }
