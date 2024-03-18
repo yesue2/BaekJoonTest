37890718de0447eacde0ee8897bfb3e3fc00f849
YeSeul <ysbsjh2464@naver.com>
2024-03-18 23:43:42 +0900

    1991 process

--- src/_240317/_1991_트리순회.java
+++ src/_240317/_1991_트리순회.java
@@ -1,4 +1,22 @@
 package _240317;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class _1991_트리순회 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String[][] node = new String[N][3];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                node[i][j] = st.nextToken();
+            }
+
+        }
+    }
 }
