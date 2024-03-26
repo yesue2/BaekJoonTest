7c7f87ca807245ce7db44f8822915cc11ecc3fa3
YeSeul <ysbsjh2464@naver.com>
2024-03-26 22:32:22 +0900

    1991 process

---
+++ src/_240326/트리1/_1991_트리순회.java
@@ -0,0 +1,13 @@
+package _240326.트리1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1991_트리순회 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
