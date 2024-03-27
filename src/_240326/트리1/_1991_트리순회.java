c6e20207952bb41c421b7f3945f8b8b4f4946217
YeSeul <ysbsjh2464@naver.com>
2024-03-27 19:50:39 +0900

    1991 process

--- src/_240326/트리1/_1991_트리순회.java
+++ src/_240326/트리1/_1991_트리순회.java
@@ -3,10 +3,13 @@ package _240326.트리1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class _1991_트리순회 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
 
 
     }
