a9c1d9f8589f9de7bfae54efce99ef175c2d8ca9
YeSeul <ysbsjh2464@naver.com>
2024-04-01 22:07:38 +0900

    1339 success

--- src/_240330/브루트포스순열/_1339_단어수학.java
+++ src/_240330/브루트포스순열/_1339_단어수학.java
@@ -3,8 +3,7 @@ package _240330.브루트포스순열;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class _1339_단어수학 {
     static int N;
@@ -18,41 +17,27 @@ public class _1339_단어수학 {
         for (int i = 0; i < N; i++) {
             str[i] = br.readLine();
         }
-        for (int i = 0; i < str.length; i++) {
-            System.out.println(str[i]);
-        }
+        result = 0;
         max();
         System.out.println(result);
     }
     static void max() {
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < str.length; i++) {
-            String[] arr = str[i].split("");  //arr[0] = G, arr[1] = C
-            for (int j = 0; j < arr.length; j++) {
-                if (hashMap.containsKey(arr[j])) {
-                    if (hashMap.get(arr[j]) < 9-j) {
-                        hashMap.remove(arr[j]);
-                        hashMap.put(arr[j], 9 - j);
-                    }
-                } else
-                    hashMap.put(arr[j], 9-j);
+        int[] alpha = new int[26];
+        for (int i = 0; i < N; i++) {
+            int tmp = (int)Math.pow(10, str[i].length()-1);
+            for (int j = 0; j < str[i].length(); j++) {
+                alpha[(int)str[i].charAt(j) - 65] += tmp;
+                tmp /= 10;
             }
         }
 
-
-        int[] add = new int[str.length];
-        for (int i = 0; i < str.length; i++) {
-            String[] arr = str[i].split("");
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < arr.length; j++) {
-                sb.append(hashMap.get(arr[j]));
-            }
-            add[i] = Integer.parseInt(sb.toString());
-        }
-        result = 0;
-        for (int i = 0; i < add.length; i++) {
-            System.out.println(add[i]);
-            result += add[i];
+        Arrays.sort(alpha);
+        int idx = 9;
+        for (int i = alpha.length-1; i >= 0; i--) {
+            if (alpha[i] == 0)
+                break;
+            result += alpha[i] * idx;
+            idx--;
         }
     }
 }
