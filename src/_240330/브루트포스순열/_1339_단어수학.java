52b226efee95b9570b7da987e0c102a960d92883
YeSeul <ysbsjh2464@naver.com>
2024-04-02 18:55:38 +0900

    1339 success

--- src/_240330/브루트포스순열/_1339_단어수학.java
+++ src/_240330/브루트포스순열/_1339_단어수학.java
@@ -22,12 +22,12 @@ public class _1339_단어수학 {
         System.out.println(result);
     }
     static void max() {
-        int[] alpha = new int[26];
-        for (int i = 0; i < N; i++) {
-            int tmp = (int)Math.pow(10, str[i].length()-1);
+        int[] alpha = new int[26];  // 0부터 25까지 (각 알파벳의 순서-1)에 맞게 저장됨
+        for (int i = 0; i < N; i++) {  // i = 0
+            int tmp = (int)Math.pow(10, str[i].length()-1);  // 10 * 10 = 100
             for (int j = 0; j < str[i].length(); j++) {
-                alpha[(int)str[i].charAt(j) - 65] += tmp;
-                tmp /= 10;
+                alpha[(int)str[i].charAt(j) - 65] += tmp;  // G = 71, 71-65 = 6(G는 7번째 알파벳) => alpha[6] = 100 저장
+                tmp /= 10;  // 다음 알파벳부터 자릿수가 하나씩 줄어들며 저장
             }
         }
 
@@ -41,3 +41,4 @@ public class _1339_단어수학 {
         }
     }
 }
+
