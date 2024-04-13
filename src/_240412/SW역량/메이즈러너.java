fe69b38c0845c7f383652f768c3c3e2a1dac09b9
YeSeul <ysbsjh2464@naver.com>
2024-04-14 03:26:34 +0900

    메이즈러너

--- src/_240412/SW역량/메이즈러너.java
+++ src/_240412/SW역량/메이즈러너.java
@@ -13,7 +13,7 @@ public class 메이즈러너 {
         }
     }
 
-    static int N, M, K, result;
+    static int N, M, K, result, msx, msy, mlx, mly;
     static int[][] miro;
     static List<Participant> pList;
     static List<Participant>[][] runner;
@@ -21,7 +21,6 @@ public class 메이즈러너 {
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     static boolean[][] visited;
-    static List<Integer> des;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -52,33 +51,46 @@ public class 메이즈러너 {
         exit[0] = Integer.parseInt(st.nextToken()) - 1;
         exit[1] = Integer.parseInt(st.nextToken()) - 1;
 
+        for (int i = 0; i < runner.length; i++) {
+            for (int j = 0; j < runner[i].length; j++) {
+                runner[i][j] = new ArrayList<>();
+            }
+        }
+
         for (int i = 0; i < pList.size(); i++) {
             Participant participant = pList.get(i);
             int x = participant.x;
             int y = participant.y;
-            if (runner[x][y] == null)
-                runner[x][y] = new ArrayList<>();
             runner[x][y].add(participant);
         }
 
         for (int i = 0; i < runner.length; i++) {
             for (int j = 0; j < runner[i].length; j++) {
-                if (runner[i][j] != null) {
-                    System.out.println(i + " " + j);
+                if (!runner[i][j].isEmpty()) {
+                    System.out.println("참가자의 i, j : " + i + " " + j);
                 }
             }
         }
-        running();
+
+        for (int i = 0; i < K; i++) {
+            if(!running()) {
+                break;
+            }
+        }
         System.out.println(result);
+        System.out.println(exit[0] + " " + exit[1]);
     }
 
-    static void running() {
+    static boolean running() {
         result = 0;
         visited = new boolean[N][N];
         move();  // 참가자 이동
         System.out.println("result : " + result);
 
-        rotation();  // 미로 회전
+        if (!rotation()) {
+            return false;
+        }
+        return true;
     }
 
     static void move() {
@@ -111,7 +123,9 @@ public class 메이즈러너 {
                     }
                 }
             } else if (d.size() == 1) {  // 최단거리가 하나일 때
+                System.out.println(d.get(0));
                 setMove(participant, d.get(0));
+
             }
         }
     }
@@ -120,79 +134,150 @@ public class 메이즈러너 {
         runner[participant.x][participant.y].remove(participant);
         participant.x += dx[i];
         participant.y += dx[i];
-        if (runner[participant.x][participant.y] == null)
-            runner[participant.x][participant.y] = new ArrayList<>();
+        System.out.println(participant.x + " " + participant.y);
         runner[participant.x][participant.y].add(participant);
         result++;
         System.out.println("움직인 방향 : " + i);
     }
 
-    static void rotation() {
-        des = new ArrayList<>();  // 각 참가자 별 최단거리 저장
-        chooseClose();
-    }
-
-    static void chooseClose() {
-        for (int i = 1; i < N; i++) {
+    static boolean rotation() {
+        setMinSquare();  // 가장 작은 정사각형 선택
+        setRotation();  // 90도 회전
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-
-            }
-        }
-        int[] dx = {-1, -1, 1, 1};
-        int[] dy = {-1, 1, 1, -1};
-        int num = 0;
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 0; j < 4; j++) {
-                int nx = exit[0] - dx[0] * i;
-                int ny = exit[1] - dy[0] * i;
-                if (nx < 0 && ny < 0) {
-                    nx = exit[0] - ;
-                    num++;
-                    continue;
-                }
-                if (nx < 0 && ny >= N) {
-                    num++;
-                    continue;
-                } else if (nx >= N && ny < 0) {
-                    num++;
-                    continue;
-                } else if (nx >= N && ny >= N) {
-                    continue;
-                }
-                switch (num) {
-                    case 0:
-                        if (checkParticipant(i, nx, ny))
-                            break;
-                        break;
-                    case 1:
-
-
-
+                if (!runner[i][j].isEmpty() && i == exit[0] && j == exit[1]) {
+                    runner[i][j].removeIf(participant -> true);
+                } else if (!runner[i][j].isEmpty()) {
+                    cnt++;
                 }
             }
         }
-
-        for (int i = 0; i < pList.size(); i++) {
-            Participant participant = pList.get(i);
-            int cx = participant.x;
-            int cy = participant.y;
-
-            des.add(Math.abs(cx - exit[0]) + Math.abs(cy - exit[1]));
+        if (cnt == 0) {
+            return false;
         }
-        int max = 0;
-        for (int i = 0; i < des.size(); i++) {
-            if (des.get(i) > max)
+        return true;
+    }
+
+    static void setMinSquare() {
+        for (int i = 1; i < N; i++) {  // 사각형의 크기
+            for (int sx = 0; sx < N - i; sx++) {   // 시작점의 x
+                int lx = sx + i;   // 끝점의 x
+
+                for (int sy = 0; sy < N - i; sy++) {   // 시작점의 y
+                    int ly = sy + i;   // 끝점의 y
+                    if (exit[0] >= sx && exit[1] >= sy && exit[0] <= lx && exit[1] <= ly) {  // 출구가 사각형 안에 있을 때
+                        for (int tsx = sx; tsx <= lx; tsx++) {
+                            for (int tsy = sy; tsy <= ly; tsy++) {
+                                if (!runner[tsx][tsy].isEmpty()) {  // 한 명 이상의 참가자가 사각형 안에 있을 때
+                                    msx = sx;
+                                    msy = sy;
+                                    mlx = lx;
+                                    mly = ly;
+                                    System.out.println("msx, msy, mlx, mly : " + msx + " " + msy + " " + mlx + " " + mly + " ");
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
-    static boolean checkParticipant (int i, int nx, int ny) {
-        for (int k = 0; k < i + 1; k++) {
-            for (int l = 0; l < i + 1; l++) {
-                if (!runner[nx+k][ny+l].isEmpty()) {
-                    return true;
+    static void setRotation() {
+        int[][] exitMap = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == exit[0] && j == exit[1]) {
+                    exitMap[i][j] = 1;
+                } else
+                    exitMap[i][j] = 0;
+            }
+        }
+
+        int length = mlx - msx + 1;  // 정사각형 한 변의 길이
+        int tmpMiro;
+        int tmpExit;
+        List<Participant> tmpRunner;
+        int bx;
+        int by;
+        int ax;
+        int ay;
+        for (int i = 0; i < length / 2; i++) {
+            int tsx = msx + i;
+            int tsy = msy + i;
+            for (int j = 0; j < length - 1; j++) {
+                tmpMiro = miro[tsx][tsy];
+                tmpExit = exitMap[tsx][tsy];
+                tmpRunner = runner[tsx][tsy];
+                runner[tsx][tsy] = new ArrayList<>();
+
+                for (int k = 0; k < length - 1; k++) {
+                    bx = tsx + k + 1;
+                    by = tsy;
+                    ax = tsx + k;
+                    ay = tsy;
+                    miro[ax][ay] = miro[bx][by];
+                    exitMap[ax][ay] = exitMap[bx][by];
+                    runner[ax][ay] = runner[bx][by];
+                }
+                for (int k = 0; k < length - 1; k++) {
+                    bx = tsx + length - 1;
+                    by = tsy + k + 1;
+                    ax = tsx + length - 1;
+                    ay = tsy + k;
+                    miro[ax][ay] = miro[bx][by];
+                    exitMap[ax][ay] = exitMap[bx][by];
+                    runner[ax][ay] = runner[bx][by];
+                }
+                for (int k = 0; k < length - 1; k++) {
+                    bx = tsx + length - 2 - k;
+                    by = tsy + length - 1;
+                    ax = tsx + length - 1 - k;
+                    ay = tsy + length - 1;
+                    miro[ax][ay] = miro[bx][by];
+                    exitMap[ax][ay] = exitMap[bx][by];
+                    runner[ax][ay] = runner[bx][by];
+                }
+                for (int k = 0; k < length - 2; k++) {
+                    bx = tsx;
+                    by = tsy + length - 2 - k;
+                    ax = tsx;
+                    ay = tsy + length - 1 - k;
+                    miro[ax][ay] = miro[bx][by];
+                    exitMap[ax][ay] = exitMap[bx][by];
+                    runner[ax][ay] = runner[bx][by];
+                }
+                miro[tsx][tsy + 1] = tmpMiro;
+                exitMap[tsx][tsy + 1] = tmpExit;
+                runner[tsx][tsy + 1] = tmpRunner;
+            }
+            for (int j = 0; j < N; j++) {   // 내구력 -1
+                for (int k = 0; k < N; k++) {
+                    if (miro[j][k] != 0) {
+                        miro[j][k]--;
+                    }
+                    if (exitMap[j][k] == 1) {
+                        exit[0] = j;
+                        exit[1] = k;
+                    }
+                }
+            }
+            for (int k = 0; k < N; k++) {
+                for (int l = 0; l < N; l++) {
+                    System.out.print(miro[k][l] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("exit : " + exit[0] + " " + exit[1]);
+            for (int k = 0; k < runner.length; k++) {
+                for (int j = 0; j < runner[k].length; j++) {
+                    if (!runner[k][j].isEmpty()) {
+                        System.out.println("참가자의 i, j : " + k + " " + j);
+                    }
                 }
             }
         }
-        return false;
     }
 }
