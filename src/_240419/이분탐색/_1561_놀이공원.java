24a07414ce3b85d7588cc9a52261892e7d77bc9a
YeSeul <ysbsjh2464@naver.com>
2024-04-20 02:27:26 +0900

    1561 success

--- src/_240419/이분탐색/_1561_놀이공원.java
+++ src/_240419/이분탐색/_1561_놀이공원.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class _1561_놀이공원 {
-    static long N, num, result;
+    static long N, result, mid;
     static int M, maxT;
     static int[] time;
     static List<Integer> startList;
@@ -36,64 +36,38 @@ public class _1561_놀이공원 {
         long left = 0;
         long right = maxT * N;
         while (left <= right) {
-            long mid = (left + right) / 2;
-            System.out.println("left, mid, right : " + left + " " + mid + " " + right);
-            num = setTime(mid);  // 놀이기구에 모든 인원이 탑승하게 되는 시간 구하기
-            System.out.println("num : " + num);
-            if (num == -1) {  // 놀에기구에 탑승한 인원이 N명보다 많을 경우
-                right = mid - 1;
-            } else if (num == -2) {  // 놀에기구에 탑승한 인원이 N명보다 적을 경우
-                left = mid + 1;
-            } else {
-                long tmp = 0;
-                for (int i = 1; i < right; i++) {
-                    tmp = setTime(num-i);
-                    if (tmp != num) {
-                        num -= i + 1;
-                    }
-                }
-                break;
+            mid = (left + right) / 2;
+            long num = countStart(mid);  // 해당 시간(mid)까지 운행 시작한 놀이기구의 수
+            if (num >= N) {  // 현재 시간에 N개 이상의 놀이기구가 운행을 시작했을 경우
+                getLastRide(mid);  // 마지막 아이가 탑승한 놀이기구 번호 구하기
+                right = mid - 1;  // 시간 줄이기
+            } else {  // 현재 시간에 N보다 적은 놀이기구가 운행을 시작했을 경우
+                left = mid + 1;  // 시간 늘리기
             }
         }
-        // mid값이 시간 정답
-        setNum(num);  // 마지막 고객이 몇 번째 놀이기구에 탔는지 반환
     }
 
-    static long setTime(long t) {
-        startList.clear();
-        long allNum = M;
+    static long countStart(long t) {
+        long cnt = M;  // 모든 놀이기구가 한 번씩은 시작
         for (int i = 0; i < M; i++) {
-            allNum += t / time[i];
-
-            long tmp = t % time[i];  // 해당 시간에 출발한 놀이기구의 번호 구하기
-            if (tmp == 0) {
-                startList.add(i+1);
-            }
+            cnt += t / time[i];
         }
-        if (allNum > N) {
-            return -1;
-        } else if (allNum < N) {
-            return -2;
-        }
-        return t;
+        return cnt;  // 해당 시간(t)까지 운행 시작한 놀이기구의 수
     }
 
-    static void setNum(long t) {
-        System.out.print("시작한 놀이기구 : ");
-        for (int i = 0; i < startList.size(); i++) {
-            System.out.print(startList.get(i) + " ");
+    static void getLastRide(long t) {
+        long cnt = M;  // 모든 놀이기구가 한 번씩은 시작
+        for (int i = 0; i < M; i++) {
+            cnt += (t - 1) / time[i];  // t-1 시간까지 운행 시작한 놀이기구의 수
         }
-        System.out.println();
-        long allNum = M;
-        for (int i = 0; i < M; i++) {  // 마지막 고객이 놀이기구를 탈 수 있는 시간보다 -1해서 이전 시간까지 탈 수 있는 사람의 수 구하기
-            allNum += (t-1) / time[i];
-        }
-        for (int i = 0; i < startList.size(); i++) {   // 이전 시간까지 탑승한 고객 수에 +1 하며 마지막 고객이 탑승한 놀이기구의 번호 구하기
-            result = startList.get(i);
-            System.out.println("result : " + result);
-            allNum++;
-            if (allNum == M)
-                return;
+
+        for (int i = 0; i < M; i++) {
+            if (t % time[i] == 0) {  // 마지막 아이가 탑승한 놀이기구의 번호를 반환
+                cnt++;
+                if (cnt == N) {
+                    result = i + 1;
+                }
+            }
         }
     }
 }
