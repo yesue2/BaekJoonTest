734e40dc107b1c3ebcfe5b8c080ffd5794e9b020
YeSeul <ysbsjh2464@naver.com>
2024-05-05 17:41:33 +0900

    2151 process

--- src/_240502/BFS/_2151_거울설치.java
+++ src/_240502/BFS/_2151_거울설치.java
@@ -13,7 +13,10 @@ public class _2151_거울설치 {
     static char[][] map;
     static int[] dx = {0, 1, -1, 0};
     static int[] dy = {1, 0, 0, -1};
+    static boolean[][] visited;
     static int[] doorXY;
+    static Queue<int[]> queue;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -40,8 +43,8 @@ public class _2151_거울설치 {
     }
 
     static void bfs() {
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[N][N];
+        queue = new LinkedList<>();
+        visited = new boolean[N][N];
         int x = doorXY[0];
         int y = doorXY[1];
         queue.offer(new int[]{x, y});
@@ -54,9 +57,31 @@ public class _2151_거울설치 {
 
             if (map[xy[0]][xy[1]] == '/') {
                 for (int i = 0; i < 2; i++) {
-                    int nx = xy[0] + dx[0];
+                    int nx = xy[0] + dx[i];
+                    int ny = xy[0] + dy[i];
+                }
+            } else if (map[xy[0]][xy[1]] == '\\') {
+                for (int i = 2; i < 4; i++) {
+                    int nx = xy[0] + dx[i];
+                    int ny = xy[0] + dy[i];
+                }
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    int nx = xy[0] + dx[i];
+                    int ny = xy[0] + dy[i];
+
                 }
             }
+        }
+    }
+    static void isCheck(int nx, int ny) {
+        if (nx < N && ny < N && nx >= 0 && ny >= 0 && map[nx][ny] != '*') {
+            visited[nx][ny] = true;
+            if (map[nx][ny] == '!') {
+                map[nx][ny] = ''
+            }
+            queue.offer(new int[]{nx, ny});
+
         }
     }
 }
