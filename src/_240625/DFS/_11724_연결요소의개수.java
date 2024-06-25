805fe756930aa91a37504dc58aeb16b39a5b137b
YeSeul <ysbsjh2464@naver.com>
2024-06-25 15:40:33 +0900

    11724 success

---
+++ src/_240625/DFS/_11724_연결요소의개수.java
@@ -0,0 +1,48 @@
+package _240625.DFS;
+
+import java.io.*;
+import java.util.*;
+
+public class _11724_연결요소의개수 {
+    static int[][] graph;
+    static boolean[] visited;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        graph = new int[N + 1][N + 1];
+        visited = new boolean[graph.length];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x][y] = 1;
+            graph[y][x] = 1;
+        }
+
+        int cnt = 0;
+        for (int i = 1; i <= N; i++) {
+            if (visited[i]) continue;
+            dfs(i);
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+
+    public static void dfs(int start) {
+        visited[start] = true;
+
+        for (int i = 1; i <= N; i++) {
+            if (graph[start][i] == 1 && !visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+}
