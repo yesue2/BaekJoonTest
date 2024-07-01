558e4bfca13120ff6a81677714168934af8f1b26
YeSeul <ysbsjh2464@naver.com>
2024-07-01 23:19:38 +0900

    1644 success

--- src/_240629/DFS/_1967_트리의지름.java
+++ src/_240629/DFS/_1967_트리의지름.java
@@ -11,25 +11,27 @@ import java.util.StringTokenizer;
 public class _1967_트리의지름 {
     static class Node {
         int num, cost;
+
         Node(int num, int cost) {
             this.num = num;
             this.cost = cost;
         }
     }
+
     static int n, result;
     static List<Node>[] graph;
     static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         n = Integer.parseInt(br.readLine());
+        graph = new ArrayList[n + 1];
 
-        graph = new ArrayList[n+1];
-
-        for (int i = 0; i < n+1; i++) {
-            graph[i] = new ArrayList<Node>();
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
         }
-
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
@@ -37,14 +39,16 @@ public class _1967_트리의지름 {
             graph[x].add(new Node(y, cost));
             graph[y].add(new Node(x, cost));
         }
+
         result = 0;
-        for (int i = 1; i <= n; i++) {
-            visited = new boolean[n+1];
-            visited[i] = true;
+        for (int i = 0; i < n; i++) {
+            visited = new boolean[n];
+            visited[i + 1] = true;
             dfs(i, 0);
         }
         System.out.println(result);
     }
+
     static void dfs(int start, int depth) {
         for (Node node : graph[start]) {
             if (!visited[node.num]) {
@@ -52,8 +56,6 @@ public class _1967_트리의지름 {
                 dfs(node.num, depth + node.cost);
             }
         }
-        if (result < depth) {
-            result = depth;
-        }
+        if (result < depth) result = depth;
     }
 }
