93d9b38b6754a1d7f18f0055aa7790589aaf7a69
YeSeul <108323785+yesue2@users.noreply.github.com>
2024-07-05 16:05:18 +0900

    [level 2] Title: 기능개발, Time: 0.15 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,6 +1,33 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        return answer;
+    public ArrayList<Integer> solution(int[] progresses, int[] speeds) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < progresses.length; i++){
+            if((100 - progresses[i]) % speeds[i] == 0)
+                progresses[i] = Integer.valueOf((100 - progresses[i]) / speeds[i]);
+            else
+                progresses[i] = Integer.valueOf((100 - progresses[i]) / speeds[i] + 1);
+        }
+        ArrayList<Integer> arr = new ArrayList<>();
+        stack.push(progresses[0]);
+        int count = 1;
+        int idx = 0;
+        for(int i = 1; i < progresses.length; i++){
+            if(stack.peek() >= progresses[i]){
+                count++;
+            }
+            else{
+                stack.push(progresses[i]);
+                idx = i;
+                arr.add(count);
+                count = 1;
+            }
+        }
+        arr.add(count);
+        return arr;
+        
+        
     }
 }
