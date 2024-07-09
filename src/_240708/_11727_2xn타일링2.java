49be8fcb94b95ac448c6ec5a183bbdd05c4aa83f
YeSeul <ysbsjh2464@naver.com>
2024-07-10 00:22:51 +0900

    11727 success

---
+++ src/_240708/_11727_2xn타일링2.java
@@ -0,0 +1,21 @@
+package _240708;
+
+import java.io.*;
+
+public class _11727_2xn타일링2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        if (n > 1) {
+            dp[2] = 3;
+        }
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 2]) % 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
