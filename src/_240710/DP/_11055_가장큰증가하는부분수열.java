7d6b9271b2ff18c69b98a612203e3d2634e17907
YeSeul <ysbsjh2464@naver.com>
2024-07-11 09:23:36 +0900

    11055 success

---
+++ src/_240710/DP/_11055_가장큰증가하는부분수열.java
@@ -0,0 +1,39 @@
+package _240710.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11055_가장큰증가하는부분수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = a[i];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (a[j] < a[i]) {
+                    dp[i] = Math.max(dp[i], a[i] + dp[j]);
+                }
+            }
+        }
+
+        int maxSum = dp[0];
+        for (int i = 1; i < n; i++) {
+            maxSum = Math.max(maxSum, dp[i]);
+        }
+        System.out.println(maxSum);
+    }
+}
