95361cdd102110abd3b1e4869912882b74e6a82a
YeSeul <ysbsjh2464@naver.com>
2024-07-12 23:50:27 +0900

    16928 success

---
+++ src/_240712/BFS/_16928_뱀과사다리게임.java
@@ -0,0 +1,71 @@
+package _240712.BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class _16928_뱀과사다리게임 {
+    static int[] board;
+    static boolean[] visited;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[101];
+        visited = new boolean[101];
+
+        for (int i = 1; i <= 100; i++) {
+            board[i] = i;
+        }
+
+        // 사다리
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            board[x] = y;
+        }
+
+        // 뱀
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            board[u] = v;
+        }
+
+        System.out.println(bfs());
+    }
+
+    static int bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {1, 0});
+        visited[1] = true;
+
+        while (!queue.isEmpty()) {
+            int[] xy = queue.poll();
+
+            for (int i = 1; i <= 6; i++) {
+                int nXY = xy[0] + i;
+
+                if (nXY > 100) continue;
+
+                if (board[nXY] == 100) return xy[1] + 1;
+
+                if (!visited[board[nXY]]) {
+                    visited[board[nXY]] = true;
+                    queue.offer(new int[] {board[nXY], xy[1] + 1});
+                }
+            }
+        }
+        return -1;
+    }
+}
