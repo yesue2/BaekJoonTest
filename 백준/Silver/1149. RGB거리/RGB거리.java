2d78b04f6d4ee6e2f810eb10c4bb119b9d7a6aea
YeSeul <108323785+yesue2@users.noreply.github.com>
2024-07-13 01:16:34 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -14,21 +14,16 @@ public class Main {
             }
         }
 
-        int[][] dp = new int[n][3];
-        // 첫 번째 집의 비용 초기화
-        dp[0][0] = homes[0][0];
-        dp[0][1] = homes[0][1];
-        dp[0][2] = homes[0][2];
 
         // 두 번째 집부터 n번째 집까지의 최소 비용 계산
         for (int i = 1; i < n; i++) {
-            dp[i][0] = homes[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
-            dp[i][1] = homes[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
-            dp[i][2] = homes[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+            homes[i][0] += Math.min(homes[i - 1][1], homes[i - 1][2]);
+            homes[i][1] += Math.min(homes[i - 1][0], homes[i - 1][2]);
+            homes[i][2] += Math.min(homes[i - 1][0], homes[i - 1][1]);
         }
 
         // 마지막 집의 최소 비용 중 최솟값 찾기
-        int result = Math.min(dp[n - 1][0], Math.min(dp[n - 1][1], dp[n - 1][2]));
+        int result = Math.min(homes[n - 1][0], Math.min(homes[n - 1][1], homes[n - 1][2]));
         System.out.println(result);
     }
 }
