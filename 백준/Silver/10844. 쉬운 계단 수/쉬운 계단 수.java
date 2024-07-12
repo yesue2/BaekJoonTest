df8567d30f0dc60b95ee8287a4ae57271f9ea765
YeSeul <108323785+yesue2@users.noreply.github.com>
2024-07-12 18:55:55 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -6,7 +6,7 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        // dp[i][j] -> 길이가 i이고 마지막 숫자가 j인 계단 수의 개수
+        // dp[i][j] => 길이가 i이고 마지막 숫자가 j인 계단 수의 개수
         long[][] dp = new long[n + 1][10];
 
         // 길이가 1일 때의 계단 수 초기화
@@ -16,6 +16,10 @@ public class Main {
 
         for (int i = 2; i <= n; i++) {
             for (int j = 0; j <= 9; j++) {
+                /* j가 0일 때와 9일 때 빼고 다음 식이 성립
+                 => dp[i][j] = dp[i-1][j-1] + dp[i-1][i+1] */
+                // j가 0일 때 => dp[i][0] = dp[i - 1][1]
+                // j가 9일 때 => dp[i][9] = dp[i - 1][8]
                 if (j > 0) {
                     dp[i][j] += dp[i - 1][j - 1];
                 }
@@ -26,7 +30,6 @@ public class Main {
             }
         }
 
-        // 길이가 N인 계단 수의 총 합
         long result = 0;
         for (int i = 0; i <= 9; i++) {
             result += dp[n][i];
