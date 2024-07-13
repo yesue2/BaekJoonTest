1d8b8a43751e9e67d9f0db2a67b3a0433bd12cce
YeSeul <ysbsjh2464@naver.com>
2024-07-13 15:27:04 +0900

    2230 success

---
+++ src/_240713/투포인터/_2230_수고르기.java
@@ -0,0 +1,37 @@
+package _240713.투포인터;
+
+import java.io.*;
+import java.util.*;
+
+public class _2230_수고르기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(a);
+
+        int s = 0;
+        int e = 0;
+        int min = Integer.MAX_VALUE;
+        while (e < n) {
+            int tmp = a[e] - a[s];
+            if (tmp >= m) {
+                min = Math.min(min, tmp);
+                s++;
+                if (s > e) {
+                    e++;
+                }
+            } else {
+                e++;
+            }
+        }
+        System.out.println(min);
+    }
+}
