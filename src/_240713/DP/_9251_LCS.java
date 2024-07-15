21b0ee278cff50ae453c65230e3e28cd290513bb
YeSeul <ysbsjh2464@naver.com>
2024-07-15 12:02:26 +0900

    9251 success

---
+++ src/_240713/DP/_9251_LCS.java
@@ -0,0 +1,14 @@
+package _240713.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _9251_LCS {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        String result = "";
+    }
+}
