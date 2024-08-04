6ea84876751312f4a504cee74a736c7df0b27046
YeSeul <ysbsjh2464@naver.com>
2024-08-04 23:19:40 +0900

    19238 process

---
+++ src/_240804/시뮬레이션/_19238_스타트택시.java
@@ -0,0 +1,28 @@
+package _240804.시뮬레이션;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _19238_스타트택시 {
+    static int n, m, sf;
+    static int[][] map;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        sf = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+}
