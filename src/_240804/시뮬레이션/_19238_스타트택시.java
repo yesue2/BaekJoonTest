9ec5cb238991f6b6f4a1e1a332e27f4e11745d41
YeSeul <ysbsjh2464@naver.com>
2024-08-10 23:57:17 +0900

    19238 process

--- src/_240804/시뮬레이션/_19238_스타트택시.java
+++ src/_240804/시뮬레이션/_19238_스타트택시.java
@@ -8,6 +8,8 @@ import java.util.StringTokenizer;
 public class _19238_스타트택시 {
     static int n, m, sf;
     static int[][] map;
+    static int[] startT;
+    static int[][] p;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
     public static void main(String[] args) throws IOException {
@@ -17,11 +19,23 @@ public class _19238_스타트택시 {
         m = Integer.parseInt(st.nextToken());
         sf = Integer.parseInt(st.nextToken());
         map = new int[n][m];
+        startT = new int[2];
+        p = new int[m][4];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < n; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        st = new StringTokenizer(br.readLine());
+        startT[0] = Integer.parseInt(st.nextToken());
+        startT[1] = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                p[i][j] = Integer.parseInt(st.nextToken());
             }
         }
     }
