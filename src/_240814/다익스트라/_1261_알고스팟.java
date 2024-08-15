1d49737148e5e9084870452be5ad038a2abc34c6
YeSeul <ysbsjh2464@naver.com>
2024-08-15 23:09:08 +0900

    1261 success

--- src/_240814/다익스트라/_1261_알고스팟.java
+++ src/_240814/다익스트라/_1261_알고스팟.java
@@ -5,10 +5,10 @@ import java.util.*;
 
 public class _1261_알고스팟 {
     static int m, n;
-    static List<int[]>[] miro;
-    static int[][] map;
+    static int[][] miro;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,14 +16,45 @@ public class _1261_알고스팟 {
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
 
-        map = new int[n][m];
+        miro = new int[n][m];
         for (int i = 0; i < n; i++) {
             String tmp = br.readLine();
             char[] tmpCh = tmp.toCharArray();
             for (int j = 0; j < m; j++) {
-                map[i][j] = tmpCh[j] - '0';
+                miro[i][j] = tmpCh[j] - '0';
             }
         }
+        System.out.println(bfs());
+    }
 
+    static int bfs() {
+        Deque<int[]> dq = new ArrayDeque<>();
+        int[][] dist = new int[n][m];  // 출발점에서 각 위치까지 최소 벽 부수기 횟수 저장
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
+
+        dq.offer(new int[]{0, 0});
+        dist[0][0] = 0;
+
+        while (!dq.isEmpty()) {
+            int[] xy = dq.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = xy[0] + dx[i];
+                int ny = xy[1] + dy[i];
+
+                if (nx >= n || ny >= m || nx < 0 || ny < 0) continue;
+
+                int nDist = dist[xy[0]][xy[1]] + miro[nx][ny];
+
+                if (nDist < dist[nx][ny]) {
+                    dist[nx][ny] = nDist;
+                    // 빈 방(0)은 덱의 앞쪽에 삽입하고, 벽(1)은 덱의 뒤쪽에 삽입 => 벽을 적게 부순 경로가 우선적으로 탐색됨
+                    if (miro[nx][ny] == 0) dq.offer(new int[]{nx, ny});
+                    else dq.offerLast(new int[]{nx, ny});
+                }
+            }
+        }
+        return dist[n - 1][m - 1];
     }
 }
